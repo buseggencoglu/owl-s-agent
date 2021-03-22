fa8722e0b37d02782dbc67971d6c95dbc7a27ddc
Mert <mert.misirlioglu@isik.edu.tr>
2021-03-22 16:19:15 +0300

    template adapted

--- owl_agent/owl_agent/urls.py
+++ owl_agent/owl_agent/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from website import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact'),
+    path('job-details/', views.job_details_view, name='job-details'),
+    path('job-listing/', views.job_listing_view, name='job-listing'),
+    path('blog/', views.blog_view, name='blog'),
+    path('blog/single/', views.single_blog_view, name='single-blog'),
+    path('elements/', views.elements_view, name='elements'),
 ]
--- owl_agent/website/views.py
+++ owl_agent/website/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home_view(request):
+    return render(request, 'website/index.html')
+
+
+def about_view(request):
+    return render(request, 'website/about.html')
+
+
+def contact_view(request):
+    return render(request, 'website/contact.html')
+
+
+def job_details_view(request):
+    return render(request, 'website/job_details.html')
+
+
+def job_listing_view(request):
+    return render(request, 'website/job_listing.html')
+
+
+def blog_view(request):
+    return render(request, 'website/blog.html')
+
+
+def single_blog_view(request):
+    return render(request, 'website/single-blog.html')
+
+
+def elements_view(request):
+    return render(request, 'website/elements.html')
