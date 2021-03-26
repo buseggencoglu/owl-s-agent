583eab152b74ebe25e6b722c501feb440741be39
Kübra Felek <kbr.flk@hotmail.com>
2021-03-26 23:10:22 +0300

    login pages added

--- owl_agent/owl_agent/urls.py
+++ owl_agent/owl_agent/urls.py
@@ -1,18 +1,3 @@
-"""owl_agent URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
@@ -28,4 +13,8 @@ urlpatterns = [
     path('blog/', views.blog_view, name='blog'),
     path('blog/single/', views.single_blog_view, name='single-blog'),
     path('elements/', views.elements_view, name='elements'),
+
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
 ]
--- owl_agent/website/views.py
+++ owl_agent/website/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+@login_required(login_url='login')
 def home_view(request):
     return render(request, 'website/index.html')
 
@@ -33,3 +39,28 @@ def single_blog_view(request):
 
 def elements_view(request):
     return render(request, 'website/elements.html')
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'website/login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
