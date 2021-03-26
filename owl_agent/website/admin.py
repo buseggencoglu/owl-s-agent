4f3125a11e03db1e31d5f82200b75eb2d019010b
Ayşe Bayık <ayse.bayik@isik.edu.tr>
2021-03-26 15:00:12 +0300

    registered models in admin.py

--- owl_agent/website/admin.py
+++ owl_agent/website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Job_Seeker_Profile,Company_Profile,CV,Application,Job_Offer,User_Profile
 
 # Register your models here.
+
+
+admin.site.register(Job_Seeker_Profile)
+admin.site.register(Company_Profile)
+admin.site.register(CV)
+admin.site.register(Application)
+admin.site.register(Job_Offer)
+admin.site.register(User_Profile)
